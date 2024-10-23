a293594168bc8ec9a029c87413333da8c23d47d2
Junha_iknow2 <hiru0926@gmail.com>
2024-10-23 11:51:24 +0900

    preprocess by token_length

--- Music/mapping.sh
+++ Music/mapping.sh
@@ -1,10 +1,10 @@
 export USE_TORCH=1
 
-# models="wietsedv/bert-base-dutch-cased  neuralmind/bert-base-portuguese-cased	aubmindlab/bert-base-arabert tohoku-nlp/bert-base-japanese kykim/bert-kor-base 
-# microsoft/codebert-base neulab/codebert-javascript neulab/codebert-java neulab/codebert-python neulab/codebert-c
+# models="bert-base-uncased bert-base-chinese bert-base-multilingual-uncased bert-base-multilingual-cased 
+# bert-base-german-cased wietsedv/bert-base-dutch-cased  neuralmind/bert-base-portuguese-cased	aubmindlab/bert-base-arabert tohoku-nlp/bert-base-japanese kykim/bert-kor-base 
+# microsoft/codebert-base microsoft/codebert-base-mlm neulab/codebert-javascript neulab/codebert-java neulab/codebert-python neulab/codebert-c
 # bertin-project/bertin-roberta-base-spanish"
-models="bert-base-multilingual-uncased bert-base-multilingual-cased 
-bert-base-german-cased"
+models="microsoft/codebert-base-mlm"
 # models="bert-base-uncased bert-base-chinese"
 
 for model in $models
--- Music/run_finetune.sh
+++ Music/run_finetune.sh
@@ -1,6 +1,7 @@
 export USE_TORCH=1
 
-# models="wietsedv/bert-base-dutch-cased  neuralmind/bert-base-portuguese-cased	aubmindlab/bert-base-arabert tohoku-nlp/bert-base-japanese kykim/bert-kor-base 
+# models="bert-base-uncased bert-base-chinese bert-base-multilingual-uncased bert-base-multilingual-cased 
+# bert-base-german-cased wietsedv/bert-base-dutch-cased  neuralmind/bert-base-portuguese-cased	aubmindlab/bert-base-arabert tohoku-nlp/bert-base-japanese kykim/bert-kor-base 
 # microsoft/codebert-base neulab/codebert-javascript neulab/codebert-java neulab/codebert-python neulab/codebert-c
 # bertin-project/bertin-roberta-base-spanish"
 

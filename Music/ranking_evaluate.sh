22958fa70b894c60f7079df214805ca7b6af61b4
Junha_iknow2 <hiru0926@gmail.com>
2024-11-05 09:19:40 +0900

    add code for evaluating frequency ranking

---
+++ Music/ranking_evaluate.sh
@@ -0,0 +1,33 @@
+export USE_TORCH=1
+task="maestro-v1"
+
+# models="bert-base-uncased bert-base-chinese bert-base-multilingual-uncased bert-base-multilingual-cased 
+# bert-base-german-cased neuralmind/bert-base-portuguese-cased tohoku-nlp/bert-base-japanese
+# microsoft/codebert-base microsoft/codebert-base-mlm neulab/codebert-javascript neulab/codebert-java neulab/codebert-python neulab/codebert-c"
+model="bert-base-uncased"
+step="best"
+seed=2020
+batch=16
+token_len="256"
+# token_lens="256"
+rankings="top middle low"
+oov=0
+for ranking in $rankings
+do
+    CUDA_VISIBLE_DEVICES=2 python evaluate.py --task $task \
+        --split test \
+        --step ${step} \
+        -b ${batch} \
+        --type pretrain \
+        --model ${model} \
+        --seed ${seed} \
+        --logdir ./log/$task \
+        --state_dict ./pth/save_model \
+        --datadir ./data/pkl \
+        --shift_table ./shift_table \
+        --token_len ${token_len} \
+        --ranking ${ranking}  \
+        --oov ${oov}
+        # --shift_table ./data/maestro-v1.0.0/maestro-v1_bert-base-uncased_token_map.pkl\
+        
+done

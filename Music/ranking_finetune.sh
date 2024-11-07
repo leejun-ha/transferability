a14763bb08ae62d47b0506f7822d9b918ca18cb0
Junha_iknow2 <hiru0926@gmail.com>
2024-11-08 02:48:54 +0900

    add freqeuncy_align mapping for each bert model

--- Music/ranking_finetune.sh
+++ Music/ranking_finetune.sh
@@ -1,30 +1,30 @@
 export USE_TORCH=1
 
-# models="bert-base-uncased bert-base-chinese bert-base-multilingual-uncased bert-base-multilingual-cased 
-# bert-base-german-cased neuralmind/bert-base-portuguese-cased 
-# tohoku-nlp/bert-base-japanese
-# microsoft/codebert-base-mlm neulab/codebert-javascript neulab/codebert-java neulab/codebert-python neulab/codebert-c
-# "
+models="bert-base-uncased bert-base-chinese
+bert-base-german-cased neuralmind/bert-base-portuguese-cased 
+tohoku-nlp/bert-base-japanese
+microsoft/codebert-base-mlm neulab/codebert-javascript neulab/codebert-java neulab/codebert-python neulab/codebert-c
+"
 
-model="bert-base-multilingual-cased"
+# model="bert-base-multilingual-cased"
 
 epoch=30
 batch=16
 token_len="256"
 # token_len="256"
-rankings="top middle low"
+ranking="align"
 
 oov=0
 
-for ranking in $rankings
+for model in $models
 do
-    CUDA_VISIBLE_DEVICES=2 python finetune.py \
+    CUDA_VISIBLE_DEVICES=1 python finetune.py \
         --model ${model}  \
         --type pretrain \
         -e ${epoch} \
         -b ${batch} \
         --token_len ${token_len} \
-        --shift_table ./shift_table \
+        --shift_table ./shift_table/freq_align \
         --ranking ${ranking} \
         --oov ${oov}
 done
